e223e6b9fdc3d616ca1154cbdb4f3541d6c6b9bb
Alex Apetrei <alexaapetrei@gmail.com>
2023-08-05 21:34:57 +0200

    fixed 404

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,4 +1,3 @@
-
 import { Head } from "$fresh/runtime.ts";
 
 export default function Error404() {
@@ -7,20 +6,20 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="px-4 py-8 mx-auto bg-orange-500">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
           <img
             class="my-6"
-            src="/logo.svg"
+            src="/bear2023.svg"
             width="128"
             height="128"
-            alt="the fresh logo: a sliced lemon dripping with juice"
+            alt="Dang Bear"
           />
           <h1 class="text-4xl font-bold">404 - Page not found</h1>
           <p class="my-4">
-            The page you were looking for doesn't exist.
+            Not here bro
           </p>
-          <a href="/" class="underline">Go back home</a>
+          <a href="/" class="underline">Go home</a>
         </div>
       </div>
     </>
